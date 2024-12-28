c861358ce9a957d992f9a92c30af9455617a26e4
SretoSV <sreto.vujicic@gmail.com>
2024-12-28 21:52:29 +0100

    Created ShopPage and implemented ShirtCard design

---
+++ CalisthenicsLeaguesFrontend/src/components/ShirtCard.tsx
@@ -0,0 +1,92 @@
+import shirtImageBlackFront from '../images/shirtImageBlackFront.png';
+import shirtImageBlackBack from '../images/shirtImageBlackBack.png';
+import shirtImageWhiteFront from '../images/shirtImageWhiteFront.png';
+import shirtImageWhiteBack from '../images/shirtImageWhiteBack.png';
+import colorImageBlack from '../images/colorImageBlack.png';
+import colorImageWhite from '../images/colorImageWhite.png';
+import styles from '../styles/ShirtCardStyle.module.css';
+import { useState, useEffect } from 'react';
+
+interface LeagueCardProps {
+    id: number,
+    leagueName: string,
+    shirtImageBlackFront: string,
+    shirtImageBlackBack: string,
+    shirtImageWhiteFront: string,
+    shirtImageWhiteBack: string,
+}
+
+export function ShirtCard(props: LeagueCardProps) {
+    const [currentShirtImage, setCurrentShirtImage] = useState<string>(shirtImageBlackBack); 
+    const [selectedView, setSelectedView] = useState<string>("front");
+    const [currentColorImage, setCurrentColorImage] = useState<boolean>(true); 
+
+    useEffect(() => {
+        if (currentColorImage && selectedView === "front") {
+            setCurrentShirtImage(shirtImageBlackFront);
+        } else if (currentColorImage && selectedView === "back") {
+            setCurrentShirtImage(shirtImageBlackBack);
+        }
+        else if (!currentColorImage && selectedView === "front") {
+            setCurrentShirtImage(shirtImageWhiteFront);
+        }else{
+            setCurrentShirtImage(shirtImageWhiteBack);
+        }
+
+    }, [selectedView]);
+
+    const handleViewChange = (view: string) => {
+        setSelectedView(view);
+    };
+
+    const handleColorImageClick = () => {
+        setCurrentColorImage(current => !current);
+        if (currentColorImage && selectedView === "front") {
+            setCurrentShirtImage(shirtImageWhiteFront);
+        } else if (currentColorImage && selectedView === "back") {
+            setCurrentShirtImage(shirtImageWhiteBack);
+        }
+        else if (!currentColorImage && selectedView === "front") {
+            setCurrentShirtImage(shirtImageBlackFront);
+        }else{
+            setCurrentShirtImage(shirtImageBlackBack);
+        }
+    };
+
+    return (
+        <div className={styles.mainDiv}>
+                <h2 className={styles.header2}>{props.leagueName}</h2>
+                <img 
+                    src={currentColorImage ? colorImageBlack : colorImageWhite}
+                    alt="color"
+                    className={styles.colorImage}
+                    onClick={handleColorImageClick}
+                    title='change color'
+                />
+            <img 
+                src={currentShirtImage}
+                alt="Shirt"
+                className={styles.leagueImage}
+            />
+            <div>
+                <input 
+                    style={{width: "20px", height: "20px"}}
+                    type="radio" 
+                    id="front" 
+                    checked={selectedView === "front"} 
+                    onChange={() => handleViewChange("front")} 
+                />
+
+                <input 
+                    style={{width: "20px", height: "20px"}}
+                    type="radio" 
+                    id="back" 
+                    checked={selectedView === "back"} 
+                    onChange={() => handleViewChange("back")} 
+                />
+            </div>
+
+            <a className={styles.applyButton} href="#">Add To Cart</a>
+        </div>
+    );
+}
--- CalisthenicsLeaguesFrontend/src/pages/ShopPage.tsx
+++ CalisthenicsLeaguesFrontend/src/pages/ShopPage.tsx
@@ -1,7 +1,77 @@
+import { useState } from "react";
+import { Navigation } from "../components/Navigation";
+import { ShirtCard } from "../components/ShirtCard";
+
 export function ShopPage(){
+
+    const [shirts, setShirts] = useState([
+        {   
+            id: 1,
+            leagueName: "Legendary",
+            shirtImageBlackFront: "league1",//cuvacu ih na backendu
+            shirtImageBlackBack: "league1",
+            shirtImageWhiteFront: "league1",
+            shirtImageWhiteBack: "league1",
+        },
+        {   
+            id: 2,
+            leagueName: "World-Class",
+            shirtImageBlackFront: "league1",
+            shirtImageBlackBack: "league1",
+            shirtImageWhiteFront: "league1",
+            shirtImageWhiteBack: "league1",
+        },
+        {   
+            id: 3,
+            leagueName: "Pro",
+            shirtImageBlackFront: "league1",
+            shirtImageBlackBack: "league1",
+            shirtImageWhiteFront: "league1",
+            shirtImageWhiteBack: "league1",
+        },
+        {   
+            id: 4,
+            leagueName: "Semi-pro",
+            shirtImageBlackFront: "league1",
+            shirtImageBlackBack: "league1",
+            shirtImageWhiteFront: "league1",
+            shirtImageWhiteBack: "league1",
+        },
+        {   
+            id: 5,
+            leagueName: "Amateur",
+            shirtImageBlackFront: "league1",
+            shirtImageBlackBack: "league1",
+            shirtImageWhiteFront: "league1",
+            shirtImageWhiteBack: "league1",
+        },
+        {   
+            id: 6,
+            leagueName: "Begginer",
+            shirtImageBlackFront: "league1",
+            shirtImageBlackBack: "league1",
+            shirtImageWhiteFront: "league1",
+            shirtImageWhiteBack: "league1",
+        },
+    ]);
+
+
     return(
         <>
-            <h2>ShopPage</h2>
+            <Navigation />
+            <div>
+                {shirts.map((shirt) => {
+                    return <ShirtCard 
+                                key={shirt.id} 
+                                id={shirt.id} 
+                                leagueName={shirt.leagueName}
+                                shirtImageBlackFront={shirt.shirtImageBlackFront}
+                                shirtImageBlackBack={shirt.shirtImageBlackBack}
+                                shirtImageWhiteFront={shirt.shirtImageWhiteFront}
+                                shirtImageWhiteBack={shirt.shirtImageWhiteBack}
+                            />
+                })}
+            </div>
         </>
     );
 }
