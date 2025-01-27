cea03486bdb2e4cb270d99973ee1b200c21823ba
SretoSV <sreto.vujicic@gmail.com>
2025-01-27 12:26:18 +0100

    ConnectionClass, User class and ICRUDDao created

---
+++ backend/CalisthenicsLeagues/Connection/ConnectionClass.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CalisthenicsLeagues.Connection
+{
+    public class ConnectionClass
+    {
+        public static string? GetConnectionString()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            return configuration.GetConnectionString("DefaultConnection");
+        }
+
+    }
+}
---
+++ backend/CalisthenicsLeagues/Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace CalisthenicsLeagues.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Image { get; set; }
+        public string Instagram { get; set; }
+        public bool Loggedin { get; set; }
+
+        public User() { }
+
+        public User(int id, string username, string name, string surname, string country, string email, string password, string image, string instagram, bool loggedin)
+        {
+            Id = id;
+            Username = username;
+            Name = name;
+            Surname = surname;
+            Country = country;
+            Email = email;
+            Password = password;
+            Image = image;
+            Instagram = instagram;
+            Loggedin = loggedin;
+        }
+    }
+}
