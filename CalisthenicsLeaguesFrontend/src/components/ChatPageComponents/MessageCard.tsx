507f9a58e3ab63af6c2a8543617c4d3e13c79c0b
SretoSV <sreto.vujicic@gmail.com>
2025-02-08 20:07:59 +0100

    MessageCard style

--- CalisthenicsLeaguesFrontend/src/components/ChatPageComponents/MessageCard.tsx
+++ CalisthenicsLeaguesFrontend/src/components/ChatPageComponents/MessageCard.tsx
@@ -1,6 +1,7 @@
 import { serverPath } from '../../functions/serverpath';
 import { useUserContext } from '../../context/UserContext';
 import styles from '../../styles/ChatPageStyles/MessageCardStyle.module.css';
+import arrowImage from '../../images/moreArrow.png';
 
 interface MessageCardProps{
     Id: number,
@@ -16,21 +17,63 @@ export function MessageCard(props: MessageCardProps){
     const { user } = useUserContext();
 
     return (
-        <div className={styles.mainDiv}>
-            <img 
-                src={serverPath()+props.UserProfilePicture}
-                alt="photo" 
-                className={styles.athleteImage}
-            />
-            <div>
-                <div>
-                    {props.User}
-                </div>
-                <div>
+        <>
+        {
+        (user?.username == props.User) ? 
+            <div className={styles.mainDiv1}>
+                <div className={styles.contentDiv}>
                     {props.Content}
+                    <div className={styles.timeAndButtonDiv}>
+                        <button className={styles.moreButton}>
+                            <img 
+                                className={styles.arrowImage}
+                                src={arrowImage} 
+                                alt="arrow" 
+                            />
+                        </button>
+                        <div className={styles.timeDiv}>12:30</div>
+                    </div>
                 </div>
-
             </div>
-        </div>
+
+            :
+
+            <div className={styles.mainDiv2}>
+                <div className={styles.nameAndContentDiv}>
+                    <img 
+                        src={serverPath()+props.UserProfilePicture}
+                        alt="photo" 
+                        className={styles.athleteImage}
+                    />
+                    <div>
+                        <div className={styles.contentDiv}>
+
+                            <div className={styles.usernameAndContentDiv}>
+                                <div>
+                                {props.User}
+                                </div>
+                                <div>
+                                {props.Content}
+                                </div>
+                                
+                            </div>
+
+                            <div className={styles.timeAndButtonDiv}>
+                                <button className={styles.moreButton}>
+                                    <img 
+                                        className={styles.arrowImage}
+                                        src={arrowImage} 
+                                        alt="arrow" 
+                                    />
+                                </button>
+                                <div className={styles.timeDiv}>12:30</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        }
+        </>
     );
 }
