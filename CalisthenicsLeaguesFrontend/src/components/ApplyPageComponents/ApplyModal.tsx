83b2d168eb8712029bc89b9374478a07335d2446
SretoSV <sreto.vujicic@gmail.com>
2025-02-09 22:38:23 +0100

    Filling in form fields if the user is logged in

--- CalisthenicsLeaguesFrontend/src/components/ApplyPageComponents/ApplyModal.tsx
+++ CalisthenicsLeaguesFrontend/src/components/ApplyPageComponents/ApplyModal.tsx
@@ -3,6 +3,7 @@ import styles from '../../styles/ApplyPageStyles/ApplyPageStyle.module.css';
 import { serverPath } from '../../functions/serverpath';
 import { handleInputChange, setLeagueIdByLeagueName } from '../../functions/formChangeFunction';
 import { useCountriesContext } from '../../context/CountriesContext';
+import { useUserContext } from '../../context/UserContext';
 
 interface ApplyModalProps {
     leagueName: string,
@@ -11,8 +12,12 @@ interface ApplyModalProps {
 }
 
 export default function ApplyModal({ onClose, show, leagueName }: ApplyModalProps) {
-
+    const { user } = useUserContext();
     const { countries } = useCountriesContext();
+    const formatDate = (dateString: string) => {
+        if (!dateString) return '';
+        return dateString.split('T')[0];
+    };
 
     const [form, setForm] = useState({
         username: '',
@@ -26,6 +31,23 @@ export default function ApplyModal({ onClose, show, leagueName }: ApplyModalProp
         instagram: '',
         league: 0,
     });
+
+    useEffect(() => {
+        if (localStorage.getItem('user')) {
+          setForm({
+            username: user?.username || '',
+            name: user?.name || '',
+            surname: user?.surname || '',
+            password: '',
+            email: user?.email || '',
+            country: user?.country || '',
+            dateOfBirth: formatDate(user?.dateOfBirth || ''),
+            youtubeLink: '',
+            instagram: user?.instagram || '',
+            league: setLeagueIdByLeagueName(leagueName) || 0,
+          });
+        }
+    }, [user]);
 
     useEffect(() => {
         setForm(prevForm => ({
@@ -74,6 +96,140 @@ export default function ApplyModal({ onClose, show, leagueName }: ApplyModalProp
     };
 
     return (
+        user ? 
+        <form className={styles.modalOverlay} onSubmit={handleSubmit}>
+            <div className={styles.modalContent}>
+            <div>
+                Put only Youtube Link, your information we already have.
+            </div>
+            <br />
+            <div className={styles.formModal}>
+                <label htmlFor="Username">Username (max 10 characters):</label>
+                <input 
+                    id="Username" 
+                    name="username"
+                    type="text" 
+                    value={form.username} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    disabled
+                    required
+                />
+
+                <label htmlFor="Password">Password:</label>
+                <input 
+                    id="Password" 
+                    type="password" 
+                    name="password"
+                    placeholder='/'
+                    value={form.password} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    disabled
+                    required
+                />
+
+                <label htmlFor="Name">Name:</label>
+                <input 
+                    id="Name" 
+                    type="text" 
+                    name="name"
+                    value={form.name} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    disabled
+                    required
+                />
+    
+                <label htmlFor="Surname">Surname:</label>
+                <input 
+                    id="Surname" 
+                    type="text" 
+                    name="surname"
+                    value={form.surname} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    disabled
+                    required
+                />
+
+                <label htmlFor="Email">Email:</label>
+                <input 
+                    id="Email" 
+                    type="email" 
+                    name="email"
+                    value={form.email} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    disabled
+                    required
+                />
+
+                <label htmlFor="country">Country:</label>
+                <input
+                    id="country"
+                    type="text"
+                    name="country"
+                    value={form.country}
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    list="countries-list"
+                    autoComplete="off"
+                    disabled
+                    required
+                />
+                <datalist id="countries-list">
+                    {countries.map((country) => (
+                    <option key={country.name.common} value={country.name.common} />
+                    ))}
+                </datalist>
+
+                <label htmlFor="dateOfBirth">Date of Birth:</label>
+                <input 
+                    id="dateOfBirth"
+                    type="date" 
+                    name="dateOfBirth"
+                    defaultValue={form.dateOfBirth || ""} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    max={new Date().toISOString().split("T")[0]} 
+                    required
+                    disabled
+                />
+
+                <label htmlFor="youtubeLink">Youtube link:</label>
+                <input 
+                    id="youtubeLink" 
+                    type="text" 
+                    name="youtubeLink"
+                    value={form.youtubeLink} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    required
+                />
+
+                <label htmlFor="instagram">Instagram (optional):</label>
+                <input 
+                    id="instagram" 
+                    type="text" 
+                    name="instagram"
+                    value={form.instagram} 
+                    onChange={(e) => handleInputChange(e, setForm)}
+                    disabled
+                />
+
+            </div>
+    
+            <div className={styles.buttonsDiv}>
+                <button 
+                    className={styles.closeButton} 
+                    type="button" 
+                    onClick={handleCancel}
+                >
+                    Cancel
+                </button>
+                <button 
+                    className={styles.applyButtonModal} 
+                    type="submit"
+                >
+                    Apply
+                </button>
+            </div>
+            </div>
+        </form>
+        :
         <form className={styles.modalOverlay} onSubmit={handleSubmit}>
             <div className={styles.modalContent}>
             <div>
